fedfbeaf3bf33cc74700fbf98c0fd19f61d231bb
Madhukar <madhukar.reddy326@gmail.com>
2021-05-09 21:37:59 +0530

    logging functionality is added

--- src/main/java/com/rgp/spring/mongodb/resource/BookController.java
+++ src/main/java/com/rgp/spring/mongodb/resource/BookController.java
@@ -2,6 +2,8 @@ package com.rgp.spring.mongodb.resource;
 
 import com.rgp.spring.mongodb.model.Book;
 import com.rgp.spring.mongodb.repository.BookRespository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,28 +12,33 @@ import java.util.Optional;
 
 @RestController
 public class BookController {
+    Logger logger = LoggerFactory.getLogger(BookController.class);
 
     @Autowired
     private BookRespository repository;
     @PostMapping("/book")
     public String saveBook(@RequestBody Book book)
     {
+        logger.trace("this is from the saveBook method()");
         repository.save(book);
         return "success with the boookId :" +book.getId();
     }
     @GetMapping("/book")
     public List<Book> getBooks()
     {
+        logger.trace("this is from the getBooks method()");
         return repository.findAll();
     }
     @GetMapping("/book/{id}")
     public Optional<Book> getBoook(@PathVariable int id)
     {
+        logger.trace("this is from the single getBoook() method()");
         return repository.findById(id);
     }
     @DeleteMapping("/book/{id}")
     public String deleteBook(@PathVariable int id)
     {
+        logger.trace("this is from the deleteBook() method()");
         repository.deleteById(id);
         return "Deleted book with Id :"+id;
     }
--- src/test/java/com/rgp/spring/mongodb/SpringMongodbApplicationTests.java
+++ src/test/java/com/rgp/spring/mongodb/SpringMongodbApplicationTests.java
@@ -1,13 +1,46 @@
 package com.rgp.spring.mongodb;
 
-import org.junit.jupiter.api.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rgp.spring.mongodb.model.Book;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 class SpringMongodbApplicationTests {
 
+	private MockMvc mockMvc;
+	@Autowired
+	private WebApplicationContext context;
+	ObjectMapper obj = new ObjectMapper();
+	@Before
+	private void setUp()
+	{
+		mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+	}
 	@Test
-	void contextLoads() {
+	public void addBookTest() throws Exception {
+		Book book = new Book();
+		book.setId(1322);
+		book.setBookName("testBook");
+		book.setAuthorName("testAuthor");
+		String jsonRequest = obj.writeValueAsString(book);
+		MvcResult result = mockMvc.perform(post("/book").content(jsonRequest)
+				.content(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(status().isOk()).andReturn();
+		String resultContent = result.getResponse().getContentAsString();
+		//Response response = obj.readValue(resultContent,)
+
 	}
 
 }
